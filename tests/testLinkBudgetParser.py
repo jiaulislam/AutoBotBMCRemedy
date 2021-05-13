214487fa2a88e60708495c0eb5aeff1c492472dd
jiaulislam <jiaulislam.ict.bd@gmail.com>
2021-05-14 00:25:08 +0600

    changed variable names in testLinkBudgetParser.py

--- tests/testLinkBudgetParser.py
+++ tests/testLinkBudgetParser.py
@@ -26,57 +26,57 @@ class LDMA_Parser(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
 
-    def parse_link_budget(self, link_ids: list, site_ids: list):
+    def parse_link_budget(self, _link_codes: list[str], _site_codes: list[str]):
         """ Parse the Link Budget """
-        if link_ids is not None:
+        if _link_codes is not None:
             parse_info = ParseLinkBudget(driver=self.driver, timeout=3)
             parse_info.login_ldma()
             parse_info.make_dir()
             with Live(panel, refresh_per_second=4):
                 try:
-                    for index, _id in enumerate(link_ids):
+                    for _index, _link_code in enumerate(_link_codes):
                         parse_info.goto_links()
-                        parse_info.insert_link_code(_id)
+                        parse_info.insert_link_code(_link_code)
                         parse_info.select_all_dropdown()
                         parse_info.click_search()
                         try:
-                            parse_info.select_found_link_code(_id)
-                            table.add_row(f"{index+1}", f"{_id}", "✅")
+                            parse_info.select_found_link_code(_link_code)
+                            table.add_row(f"{(_index+1)}", f"{_link_code}", "✅")
                         except TimeoutException:
-                            table.add_row(f"{index+1}", f"{_id}", "❌")
+                            table.add_row(f"{(_index+1)}", f"{_link_code}", "❌")
                             continue
                     # parse_info.export_pdf_file(id) # Export As PDF
-                    parse_info.export_file(_id)  # Export As HTML
+                    parse_info.export_file(_link_code)  # Export As HTML
                     # parse_info.export_word_file(id) # Export As DOC
                     # parse_info.delete_html_file(id) # Delete the Exported HTML file
                     parse_info.logout_ldma()
                     self.driver.quit()
-                except Exception as e:
-                    print(e)
+                except Exception as error:
+                    print(error)
         else:
             parse_info = ParseLinkBudget(driver=self.driver, timeout=3)
             parse_info.login_ldma()
             parse_info.make_dir()
 
-            for site in site_ids:
+            for _site_code in _site_codes:
                 parse_info.goto_links()
                 parse_info.select_all_dropdown()
-                parse_info.insert_site_code_1(site)
+                parse_info.insert_site_code_1(_site_code)
                 parse_info.click_search()
                 if parse_info.is_available_site_1():
                     LINK_ID = parse_info.get_link_id()
-                    parse_info.search_lb_with_sitecode(site)
+                    parse_info.search_lb_with_sitecode(_site_code)
                     parse_info.export_file(LINK_ID)
                     continue
                 parse_info.clear_site_code_1()
-                parse_info.insert_site_code_2(site)
+                parse_info.insert_site_code_2(_site_code)
                 parse_info.click_search()
                 if parse_info.is_available_site_2():
                     LINK_ID = parse_info.get_link_id()
-                    parse_info.search_lb_with_sitecode(site)
+                    parse_info.search_lb_with_sitecode(_site_code)
                     parse_info.export_file(LINK_ID)
                     continue
                 else:
-                    print(f"{Colors.FAIL}{site} : Link Budget not closed.{Colors.ENDC}")
+                    print(f"{Colors.FAIL}{_site_code} : Link Budget not closed.{Colors.ENDC}")
             parse_info.logout_ldma()
             self.driver.quit()
