248915a5c8506e9bbcef1a4d6580a760108dc4a9
w4nn4b3cod3r <kmjiaulislam@gmail.com>
2021-05-16 12:12:52 +0600

    Main Menu Visual effect changed

--- prettify/driver_prettify.py
+++ prettify/driver_prettify.py
@@ -3,13 +3,15 @@ from rich.layout import Layout
 from rich import box, print
 from rich.prompt import Prompt
 from rich.panel import Panel
-from .prettify_ldma import Header
+from .prettify_ldma import Header, make_sponsor_message
 
 class MenuLayout:
 
     def __init__(self):
         self._layout = Layout()
-        self._table = Table(title="ACTIONS", expand=True, show_lines=True, box=box.SQUARE)
+        self._table = Table(title="AUTOBOT MENU", expand=True, 
+                            show_lines=True, box=box.SQUARE_DOUBLE_HEAD,
+                            title_style="#0000ff italic")
 
     def __rich__(self):
         self._layout.split(
@@ -18,13 +20,13 @@ class MenuLayout:
         )
 
         self._layout["body"].split_column(
-            Layout(name="Should_be_unused", ratio=1),
+            Layout(name="mid_section", ratio=2),
             Layout(name="table", ratio=3)
         )
 
         # Tables
-        self._table.add_column("Action Button", justify="center", header_style="bold green", no_wrap=True, style="#3be13b")
-        self._table.add_column("Action Description", justify="center", header_style="bold cyan", no_wrap=True, style="bold rgb(59,59,225)")
+        self._table.add_column("Action Button", justify="center", header_style="#3be13b", no_wrap=True, style="#3be13b")
+        self._table.add_column("Action Description", justify="center", header_style="bold cyan", no_wrap=True, style="cyan")
 
         self._table.add_row("1", "CREATE NCR 🧩")
         self._table.add_row("2", "CLOSE NCR  🎯")
@@ -33,9 +35,10 @@ class MenuLayout:
         self._table.add_row("0", "EXIT AUTOBOT  ⚔")
 
         self._layout["head"].update(Header("WELCOME TO AUTOBOT"))
+        self._layout["mid_section"].update(make_sponsor_message())
         self._layout["table"].update(self._table)
         return self._layout
 
 def get_menu_choice() -> int:
-    choice = Prompt.ask("Enter choice: ", choices=['1', '2', '3', '4', '0'])
+    choice = Prompt.ask("Enter choice")
     return int(choice)
