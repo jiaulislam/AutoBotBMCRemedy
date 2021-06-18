709a4bc46985fdfe1d76be998b93bb044647e707
jiaulislam <jiaulislam.ict.bd@gmail.com>
2021-06-18 16:07:46 +0600

    excel rewriting

--- excel/__init__.py
+++ excel/__init__.py
@@ -2,8 +2,7 @@ from abc import ABC, abstractmethod
 from typing import Dict
 
 
-class Excel(ABC):
-
+class IExcel(ABC):
     @abstractmethod
     def get_row(self, row_num: int) -> Dict[str, str]:
         pass
--- excel/excel.py
+++ excel/excel.py
@@ -5,13 +5,12 @@ from openpyxl.reader.excel import load_workbook
 from openpyxl.workbook.workbook import Workbook
 from openpyxl.worksheet.worksheet import Worksheet
 from rich.prompt import Confirm
-from excel import Excel
+from excel import IExcel
 
-EXCEL_SHEET_NAME = 'MAIN'
+EXCEL_SHEET_NAME = "MAIN"
 
 
-class ExcelWork(Excel):
-
+class ExcelWork(IExcel):
     def __init__(self, file: str) -> None:
         self._file: str = file
 
@@ -23,11 +22,17 @@ class ExcelWork(Excel):
                 with open(self._file, "a+"):
                     break
             except IOError:
-                if Confirm.ask("File is in used mode. Is it closed ?", default="(y)", show_default=True):
+                if Confirm.ask(
+                    "File is in used mode. Is it closed ?",
+                    default="(y)",
+                    show_default=True,
+                ):
                     break
                 pass
 
-        self._wb: Workbook = load_workbook(filename=self._file, read_only=False, keep_vba=False, data_only=True)
+        self._wb: Workbook = load_workbook(
+            filename=self._file, read_only=False, keep_vba=False, data_only=True
+        )
         self._ws: Worksheet = self.__change_sheet(EXCEL_SHEET_NAME)
 
     def __change_sheet(self, _sheet_name: str) -> Worksheet:
@@ -37,26 +42,28 @@ class ExcelWork(Excel):
         _row_data = {}
 
         for _cell_chr in range(65, 76):
-            _cell_idx = f'{chr(_cell_chr)}{row_num}'
-            if chr(_cell_chr) == 'F':
-                _row_data[chr(_cell_chr)] = ",".join(make_data.split_string(self._ws[_cell_idx].value))
+            _cell_idx = f"{chr(_cell_chr)}{row_num}"
+            if chr(_cell_chr) == "F":
+                _row_data[chr(_cell_chr)] = ",".join(
+                    make_data.split_string(self._ws[_cell_idx].value)
+                )
                 continue
             _row_data[_cell_idx[0]] = self._ws[_cell_idx].value
 
         return _row_data
 
     def insert_cr(self, row_num: int, cr_number: str) -> None:
-        _cell = f'J{row_num}'
+        _cell = f"J{row_num}"
         self._ws[_cell] = cr_number
 
     def get_last_row(self) -> int:
         _last_row = 0
 
         for row in range(1, self._ws.max_row):
-            if self._ws[f'A{row}'].value is None or self._ws[f'A{row}'] == '0':
+            if self._ws[f"A{row}"].value is None or self._ws[f"A{row}"] == "0":
                 break
             _last_row += 1
-        
+
         return _last_row
 
     def save(self) -> None:
